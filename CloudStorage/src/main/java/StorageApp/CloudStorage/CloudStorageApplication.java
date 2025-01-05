eeff5b21311b147ffdd35e5c3280c3c56b08a1c3
V1adislav28 <vladislovekrpzv@yandex.ru>
2025-01-05 23:14:09 +0300

    init commit

---
+++ CloudStorage/src/main/java/StorageApp/CloudStorage/CloudStorageApplication.java
@@ -0,0 +1,13 @@
+package StorageApp.CloudStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStorageApplication.class, args);
+	}
+
+}
