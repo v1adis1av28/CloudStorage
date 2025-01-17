033199b7c5d01cf5f2dcf408afafc53da3c390a9
V1adislav28 <vladislovekrpzv@yandex.ru>
2025-01-17 19:58:43 +0300

    drop alishev implementation

--- CloudStorage/src/main/java/config/SecurityConfig.java
+++ CloudStorage/src/main/java/config/SecurityConfig.java
@@ -1,25 +1,18 @@
 package config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import security.AuthProviderImpl;
 
 @EnableWebSecurity
 @Component
-public class SecurityConfig extends SecurityConfigurerAdapter {
+public class SecurityConfig{
 
-    private final AuthProviderImpl authProvider;
-
-    @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
-    }
-
-    //Настраивание аутентификации
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
-    }
 }
